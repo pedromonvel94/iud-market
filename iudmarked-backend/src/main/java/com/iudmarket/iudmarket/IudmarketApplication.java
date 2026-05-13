2e259b658c77a4a9773b44a17ac763d4872d77f6
pedromonvel94 <pedro.montoya@est.iudigital.edu.co>
2026-05-13 14:38:47 -0500

    first commit

---
+++ iudmarked-backend/src/main/java/com/iudmarket/iudmarket/IudmarketApplication.java
@@ -0,0 +1,13 @@
+package com.iudmarket.iudmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IudmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IudmarketApplication.class, args);
+	}
+
+}
---
+++ iudmarked-backend/src/test/java/com/iudmarket/iudmarket/IudmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iudmarket.iudmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IudmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
